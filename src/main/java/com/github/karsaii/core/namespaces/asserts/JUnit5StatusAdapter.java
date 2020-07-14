76ad48f6f57cf57008e42192ff8d5f9d54da7428
Istvan Karsai <istvan.karsai.public@gmail.com>
2020-07-15 01:26:41 +0200

    Removed generic wholly, as type incompatibilites still were present. (#16)

--- src/main/java/com/github/karsaii/core/namespaces/asserts/JUnit5StatusAdapter.java
+++ src/main/java/com/github/karsaii/core/namespaces/asserts/JUnit5StatusAdapter.java
@@ -8,43 +8,43 @@ import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public interface JUnit5StatusAdapter {
-    static Consumer<Data<Object>> doAssert(TriConsumer<Boolean, Boolean, String> assertion, Boolean expected, String message) {
+    static Consumer<Data> doAssert(TriConsumer<Boolean, Boolean, String> assertion, Boolean expected, String message) {
         return data -> assertion.accept(expected, data.status, message);
     }
 
-    static Consumer<Data<Object>> doAssert(TriConsumer<Boolean, Boolean, String> assertion, Boolean expected) {
+    static Consumer<Data> doAssert(TriConsumer<Boolean, Boolean, String> assertion, Boolean expected) {
         return data -> assertion.accept(expected, data.status, data.message.toString());
     }
 
-    static Consumer<Data<Object>> doAssert(BiConsumer<Boolean, String> assertion, String message) {
+    static Consumer<Data> doAssert(BiConsumer<Boolean, String> assertion, String message) {
         return data -> assertion.accept(data.status, message);
     }
 
-    static Consumer<Data<Object>> doAssert(BiConsumer<Boolean, String> assertion) {
+    static Consumer<Data> doAssert(BiConsumer<Boolean, String> assertion) {
         return data -> assertion.accept(data.status, data.message.toString());
     }
 
-    static Consumer<Data<Object>> doAssertSupplier(TriConsumer<Boolean, Boolean, Supplier<String>> assertion, Boolean expected, Supplier<String> message) {
+    static Consumer<Data> doAssertSupplier(TriConsumer<Boolean, Boolean, Supplier<String>> assertion, Boolean expected, Supplier<String> message) {
         return data -> assertion.accept(expected, data.status, message);
     }
 
-    static Consumer<Data<Object>> doAssertSupplier(TriConsumer<Boolean, Boolean, Supplier<String>> assertion, Boolean expected) {
+    static Consumer<Data> doAssertSupplier(TriConsumer<Boolean, Boolean, Supplier<String>> assertion, Boolean expected) {
         return data -> assertion.accept(expected, data.status, data.message::toString);
     }
 
-    static Consumer<Data<Object>> doAssertSupplier(BiConsumer<Boolean, Supplier<String>> assertion, Supplier<String> message) {
+    static Consumer<Data> doAssertSupplier(BiConsumer<Boolean, Supplier<String>> assertion, Supplier<String> message) {
         return data -> assertion.accept(data.status, message);
     }
 
-    static Consumer<Data<Object>> doAssertSupplier(BiConsumer<Boolean, Supplier<String>> assertion) {
+    static Consumer<Data> doAssertSupplier(BiConsumer<Boolean, Supplier<String>> assertion) {
         return data -> assertion.accept(data.status, data.message::toString);
     }
 
-    static Consumer<Data<Object>> doAssert(BiConsumer<Boolean, Boolean> assertion, Boolean expected) {
+    static Consumer<Data> doAssert(BiConsumer<Boolean, Boolean> assertion, Boolean expected) {
         return data -> assertion.accept(expected, data.status);
     }
 
-    static Consumer<Data<Object>> doAssert(Consumer<Boolean> assertion) {
+    static Consumer<Data> doAssert(Consumer<Boolean> assertion) {
         return data -> assertion.accept(data.status);
     }
 }
